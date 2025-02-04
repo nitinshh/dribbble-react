963779fd65fe29ba45495e07a4a4b09e4b3221cb
nitinshh <nitinsharma30302@gmail.com>
2025-02-04 15:18:46 +0530

    Comment out allowedHosts configuration in Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   allowedHosts: ['da82-2401-4900-8137-eed9-a167-3169-1104-4727.ngrok-free.app'],
+  // },
 })
